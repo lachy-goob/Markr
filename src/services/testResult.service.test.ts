de69bd0d8eb223c655561af49446c5ef7cf3cdc5
lachy-goob <=>
2025-05-11 21:11:58 +1000

    test: Added testResult service
    - Ensured that all Data is expressed in Percentages
    - Ensured it can handle 0 as input

---
+++ src/services/testResult.service.test.ts
@@ -0,0 +1,73 @@
+import { calculateAggregateResults } from "./testResult.service";
+import * as studentTestResultRepository from "../repository/studentTestResult.repository";
+import {
+  afterEach,
+  beforeEach,
+  describe,
+  expect,
+  it,
+  MockInstance,
+  vi,
+} from "vitest";
+
+describe("calculateAggregateResults", () => {
+  let mockFindAllByTestId: MockInstance;
+
+  beforeEach(() => {
+    mockFindAllByTestId = vi.spyOn(
+      studentTestResultRepository,
+      "findAllByTestIdSorted"
+    );
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  it("should return null if no results are found", async () => {
+    mockFindAllByTestId.mockResolvedValueOnce([]);
+    const result = await calculateAggregateResults("testId");
+    expect(result).toBeNull();
+  });
+
+  it("should calculate aggregate results correctly", async () => {
+    mockFindAllByTestId.mockResolvedValue([
+      { obtainedMarks: 8, availableMarks: 10 },
+      { obtainedMarks: 9, availableMarks: 10 },
+      { obtainedMarks: 7, availableMarks: 10 },
+    ]);
+
+    const results = await calculateAggregateResults("test");
+
+    //Note: All these are in percentages
+    expect(results).toEqual({
+      mean: 80,
+      stddev: 8.2,
+      min: 70,
+      max: 90,
+      p25: 75,
+      p50: 80,
+      p75: 85,
+      count: 3,
+    });
+  });
+
+  it("should handle the case where overallTestMarksAvailable is 0", async () => {
+    mockFindAllByTestId.mockResolvedValue([
+      { obtainedMarks: 0, availableMarks: 0 },
+      { obtainedMarks: 0, availableMarks: 0 },
+    ]);
+
+    const result = await calculateAggregateResults("test");
+    expect(result).toEqual({
+      mean: 0,
+      stddev: 0,
+      min: 0,
+      max: 0,
+      p25: 0,
+      p50: 0,
+      p75: 0,
+      count: 2,
+    });
+  });
+});
